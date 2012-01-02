076e6911f33e4621c7a1150495c8733fc06a6372
Sander Latour <latour@uva.nl>
2012-01-02 19:29:37 +0100

    1) Uses components declared in a separate file 2) Shows beta version of lecture details on select 3) Shows schedule on back 4) Deselects previous selected lecture when returning to list

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ Ext.application({
 	name: 'AWESOMEIT2012',
 	appFolder: 'app',
 	models: ['app.model.Lecture','app.model.Speaker'],
-	launch: function(){
-		Ext.create('Ext.TabPanel', {
-			fullscreen: true,
+	launch: function(){ 
+    init_components();
+
+    // Create menu
+    COMP_MAIN_MENU = Ext.create('Ext.TabPanel', {
+      fullscreen: true,
       indicator: false,
       tabBar:{
         docked:'bottom',
@@ -14,41 +17,29 @@ Ext.application({
           direction: "horizontal"
         }
       },
-			items: [
-				{
-					title: 'Schedule',
-					iconCls: 'time',
-					xtype: "list",
-					store: 'lectureStore',
-					itemTpl: '{title}',
-          grouped: true,
-          listeners: {
-            select: function(){
-              alert("Select!");
-            }
-          }
-				},
-				{
-					title: 'Contact',
-					iconCls: 'info',
-					html: 'Contact information'
-				},
-				{
-					title: 'Events',
-					iconCls: 'bookmarks',
-					html: 'Upcoming Events'
-				},
-				{
-					title: 'Sponsors',
-					iconCls: 'favorites',
-					html: 'Our sponsors'
-				},
-				{
-					title: 'Vacancies',
+      items: [
+        COMP_TAB_SCHEDULE,
+        {
+          title: 'Contact',
+          iconCls: 'info',
+          html: 'Contact information'
+        },
+        {
+          title: 'Events',
+          iconCls: 'bookmarks',
+          html: 'Upcoming Events'
+        },
+        {
+          title: 'Sponsors',
+          iconCls: 'favorites',
+          html: 'Our sponsors'
+        },
+        {
+          title: 'Vacancies',
           iconCls: 'team',
-					html: 'Job and internship opportunities.'
-				}
-			]
-		});
+          html: 'Job and internship opportunities.'
+        }
+      ]
+    });
 	}
 });
