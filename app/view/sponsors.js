5a94ec19d8221c2a5cad56de033fd5d81cf14ccc
Sander Latour <latour@uva.nl>
2012-01-04 21:38:43 +0100

    Added sponsor components

---
+++ app/view/sponsors.js
@@ -0,0 +1,73 @@
+function init_sponsor_components(){
+  /*********************************
+   * Components for the tab SPONSORS *
+   *********************************/
+  COMP_SPONSORS_LIST = Ext.create('Ext.List',{
+    xtype: 'list',
+    store: 'sponsorStore',
+    itemTpl: '{title}',
+    grouped: true,
+    listeners: {
+      select: function(){
+        var record = this.getSelected();
+        COMP_SPONSORS_DETAILS_CONTENT.setData(
+          record.items[0].data
+        );
+        COMP_TAB_SPONSORS.setActiveItem(1)
+      },
+      scope: COMP_SPONSORS_LIST
+    }
+  });
+
+  COMP_SPONSORS_DETAILS_CONTENT = Ext.create('Ext.Panel', {
+    tpl: TPL_SPONSORS_DETAILS
+  });
+
+  COMP_SPONSORS_DETAILS = Ext.create('Ext.Panel', {
+    disabled: true,
+    scrollable: {
+      direction: 'vertical'
+    },
+    items: [
+      {
+        docked : 'top',
+        xtype: 'toolbar',
+        title: 'Sponsor page',
+        items: [
+          {
+            text: 'Back',
+            handler: function(b,e){
+              COMP_SPONSORS_LIST.deselect(
+                COMP_SPONSORS_LIST.getLastSelected()
+              );
+              COMP_TAB_SPONSORS.getLayout().getAnimation().setReverse(
+                true
+              );
+              COMP_TAB_SPONSORS.setActiveItem(0)
+              COMP_TAB_SPONSORS.getLayout().getAnimation().setReverse(
+                false
+              );
+            }
+          }
+        ]
+      },
+      COMP_SPONSORS_DETAILS_CONTENT
+    ]
+  });
+
+  COMP_TAB_SPONSORS = Ext.create('Ext.Panel', {
+    title: 'Sponsors',
+    iconCls: 'favorites',
+    layout: {
+      type: 'card',
+      animation: {
+          type: 'slide',
+          direction: 'left'
+      }
+    },
+    items: [
+      COMP_SPONSORS_LIST,
+      COMP_SPONSORS_DETAILS
+    ]
+  });
+}
