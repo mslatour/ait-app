083135608ae8093befc43707918b2a0e8e49d220
Sander Latour <latour@uva.nl>
2012-01-06 16:10:38 +0100

    Small attempt

--- app/view/sponsors.js
+++ app/view/sponsors.js
@@ -13,9 +13,8 @@ function init_sponsor_components(){
         COMP_SPONSORS_DETAILS_CONTENT.setData(
           record.items[0].data
         );
-        COMP_SPONSORS_DETAILS_CONTENT.doLayout();
+        COMP_SPONSORS_DETAILS_CONTENT2.show();
         COMP_TAB_SPONSORS.setActiveItem(1);
-        COMP_TAB_SPONSORS.doLayout();
       },
       scope: COMP_SPONSORS_LIST
     }
