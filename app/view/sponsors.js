b5032d378d3a846adfdc55342f88ba3201aacd8b
Sander Latour <latour@uva.nl>
2012-01-08 22:53:25 +0100

    Implemented the new design wireframe for sponsors

--- app/view/sponsors.js
+++ app/view/sponsors.js
@@ -10,7 +10,7 @@ function init_sponsor_components(){
     listeners: {
       select: function(){
         var record = this.getSelected();
-        COMP_SPONSORS_DETAILS_CONTENT.setData(
+        COMP_SPONSORS_DETAILS_CONTENT_INFO.setData(
           record.items[0].data
         );
         COMP_TAB_SPONSORS.setActiveItem(1);
@@ -19,23 +19,34 @@ function init_sponsor_components(){
     }
   });
 
-  COMP_SPONSORS_DETAILS_CONTENT = Ext.create('Ext.Panel', {
+  COMP_SPONSORS_DETAILS_CONTENT_HEADER = Ext.create('Ext.Panel', {
+    html: "Header info"
+  });
+  
+  COMP_SPONSORS_DETAILS_CONTENT_INFO = Ext.create('Ext.Panel', {
     tpl: TPL_SPONSOR_DETAILS
   });
 
-  COMP_SPONSORS_DETAILS_CONTAINER = Ext.create('Ext.Panel', {
+  COMP_SPONSORS_DETAILS_CONTENT_EVENTS = Ext.create('Ext.List', {
+    store: 'eventStore',
+    itemTpl: '{name}'
+  });
+
+  COMP_SPONSORS_DETAILS_CONTENT_VACANCIES = Ext.create('Ext.Panel', {
+    html: "No vacancies"
+  });
+          
+  COMP_SPONSORS_DETAILS_CONTENT = Ext.create('Ext.Panel', {
     layout: "card",
     items: [
-      {
-        html: "card 1"
-      },
-      COMP_SPONSORS_DETAILS_CONTENT
+      COMP_SPONSORS_DETAILS_CONTENT_INFO,
+      COMP_SPONSORS_DETAILS_CONTENT_EVENTS,
+      COMP_SPONSORS_DETAILS_CONTENT_VACANCIES
     ]
   });
 
-  COMP_SPONSORS_DETAILS_CONTAINER.setActiveItem(1);
-
   COMP_SPONSORS_DETAILS = Ext.create('Ext.Panel', {
+    layout: "vbox",
     items: [
       {
         docked : 'top',
@@ -59,8 +70,41 @@ function init_sponsor_components(){
           }
         ]
       },
-      COMP_SPONSORS_DETAILS_CONTAINER
+      COMP_SPONSORS_DETAILS_CONTENT_HEADER,
+      {
+        layout: "hbox",
+        items: [
+          {
+            layout: "vbox",
+            items: [
+              {
+                xtype: "button",
+                text: "Info",
+                handler: function(){
+                  COMP_SPONSORS_DETAILS_CONTENT.setActiveItem(0);
+                }
+              },
+              {
+                xtype: "button",
+                text: "Events",
+                handler: function(){
+                  COMP_SPONSORS_DETAILS_CONTENT.setActiveItem(1);
+                }
+              },
+              {
+                xtype: "button",
+                text: "Vacancies",
+                handler: function(){
+                  COMP_SPONSORS_DETAILS_CONTENT.setActiveItem(2);
+                }
+              }
+            ]
+          },
+          COMP_SPONSORS_DETAILS_CONTENT_CONTAINER
+        ]
+      }
     ]
+
   });
 
   COMP_TAB_SPONSORS = Ext.create('Ext.Panel', {
@@ -75,8 +119,7 @@ function init_sponsor_components(){
     },
     items: [
       COMP_SPONSORS_LIST,
-      //COMP_SPONSORS_DETAILS
-      COMP_SPONSORS_DETAILS_CONTAINER
+      COMP_SPONSORS_DETAILS
     ]
   });
 }
