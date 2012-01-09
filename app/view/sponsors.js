5a578b8c38fcb4a15e9b441fb1546d2d547610a3
Sander Latour <latour@uva.nl>
2012-01-09 01:49:18 +0100

    Attempts to show the sponsor menu

--- app/view/sponsors.js
+++ app/view/sponsors.js
@@ -13,7 +13,7 @@ function init_sponsor_components(){
         COMP_SPONSORS_DETAILS_INFO_CONTENT.setData(
           record.items[0].data
         );
-        (COMP_SPONSORS_DETAILS.getItems())[1].setData(
+        ((COMP_SPONSORS_DETAILS.getItems())[1]).setData(
           record.items[0].data
         );
         COMP_TAB_SPONSORS.setActiveItem(1);
