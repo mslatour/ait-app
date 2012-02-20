de2ec2857be00162dbf816d4777e517f8cc845ec
Sander Latour <latour@uva.nl>
2012-02-20 17:06:23 +0100

    Now first loads the required library

--- app/view/templates.js
+++ app/view/templates.js
@@ -1,56 +1,59 @@
-var TPL_SCHEDULE_DETAILS = new Ext.XTemplate(
-  '<div class="lecture-details">',
-    '<div class="speaker">',
-      '<span class="name">',
-        '{speaker.firstname} {speaker.surname}', 
-      '</span>',
-      '<span class="affiliation">', 
-        '{speaker.affiliation}', 
-      '</span>',
-      '<span class="url">',
-        '{speaker.url}',
-      '</span>',
-      '<div class="description">',
-        '{speaker.description}', 
+var TPL_SCHEDULE_DETAILS, TPL_EVENTS_DETAILS, TPL_SPONSOR_DETAILS;
+Ext.require("Ext.XTemplate", function(){
+  TPL_SCHEDULE_DETAILS = new Ext.XTemplate(
+    '<div class="lecture-details">',
+      '<div class="speaker">',
+        '<span class="name">',
+          '{speaker.firstname} {speaker.surname}', 
+        '</span>',
+        '<span class="affiliation">', 
+          '{speaker.affiliation}', 
+        '</span>',
+        '<span class="url">',
+          '{speaker.url}',
+        '</span>',
+        '<div class="description">',
+          '{speaker.description}', 
+        '</div>',
       '</div>',
+    '<span class="title">{title}</span>',
+    '<span class="time">{time}</span>',
+    '<div class="description">{description}</div>',
     '</div>',
-  '<span class="title">{title}</span>',
-  '<span class="time">{time}</span>',
-  '<div class="description">{description}</div>',
-  '</div>',
-  {}
-);
+    {}
+  );
 
-var TPL_EVENTS_DETAILS = new Ext.XTemplate(
-  '<div class="event-details">',
-     '<span class="title">{title}</span>',
-     '<span class="date">{date}</span>',
-     '<div class="description">{description}</div>',
-     '<div class="url"><a href="{url}">{url}</a></div>',
-     '<div class="host">',
-       '<span class="name">',
-         '{host.name}', 
-       '</span>',
-       '<span class="url">', 
-         '<a href="{host.url}">{host.url}</a>', 
-       '</span>',
-       '<span class="email">', 
-         '<a href="mailto:{host.email}">{host.email}</a>', 
-       '</span>',
-       '<div class="description">',
-         '{host.description}', 
+  TPL_EVENTS_DETAILS = new Ext.XTemplate(
+    '<div class="event-details">',
+       '<span class="title">{title}</span>',
+       '<span class="date">{date}</span>',
+       '<div class="description">{description}</div>',
+       '<div class="url"><a href="{url}">{url}</a></div>',
+       '<div class="host">',
+         '<span class="name">',
+           '{host.name}', 
+         '</span>',
+         '<span class="url">', 
+           '<a href="{host.url}">{host.url}</a>', 
+         '</span>',
+         '<span class="email">', 
+           '<a href="mailto:{host.email}">{host.email}</a>', 
+         '</span>',
+         '<div class="description">',
+           '{host.description}', 
+         '</div>',
        '</div>',
-     '</div>',
-  '</div>'
-);
+    '</div>'
+  );
 
-var TPL_SPONSOR_DETAILS = new Ext.XTemplate(
-  '<div class="sponsor-details">',
-     '<span class="name">{name}</span>',
-     '<div class="description">{description}</div>',
-     '<span class="url"><a href="{url}">{url}</a></span>',
-     '<span class="email">', 
-       '<a href="mailto:{email}">{email}</a>', 
-     '</span>',
-  '</div>'
-);
+  TPL_SPONSOR_DETAILS = new Ext.XTemplate(
+    '<div class="sponsor-details">',
+       '<span class="name">{name}</span>',
+       '<div class="description">{description}</div>',
+       '<span class="url"><a href="{url}">{url}</a></span>',
+       '<span class="email">', 
+         '<a href="mailto:{email}">{email}</a>', 
+       '</span>',
+    '</div>'
+  );
+});
