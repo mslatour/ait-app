ac567f9496086fa66bef910f4b7ffc0eb21dc49d
Sander Latour <latour@uva.nl>
2012-02-25 21:36:49 +0100

    Splash page try out 7

--- app.js
+++ app.js
@@ -12,11 +12,13 @@ Ext.application({
     SPLASH = Ext.create('Ext.Panel', {
       floating: true,
       model: true,
+      width: "80%",
+      height: "80%",
       centered: true,
       items: [{
         xtype: "image",
         src: "splash-phone.png",
-        width: "100%"
+        style: "max-width: 100%"
       }]
     });
     SPLASH.show('pop');
