b6e612ada4b0d90f62d80a399dcc832fda65e9a0
Sander Latour <latour@uva.nl>
2012-02-25 21:14:21 +0100

    Splash page try out 4

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ Ext.application({
       fullscreen: true,
       html: "SPLASH!"
     });
-    setTimeout("alert(1); SPLASH.hidden = true; SPLASH.fullscreen = false;", 2000);
+    setTimeout("alert(1); SPLASH.setHidden(true);", 2000);
     // Create menu
     COMP_MAIN_MENU = Ext.create('Ext.TabPanel', {
       fullscreen: true,
